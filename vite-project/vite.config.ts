0a0c557acd1ac3b90418096d20d4ed6f11a853dd
fadel-elias <fadel.elias@estudante.ufjf.br>
2025-12-08 18:05:55 -0300

    fix: conserta gh pages

--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ufjf-dcc207-2025-3-a-sem01-amigos-do-sao-pedro-1/',
+  base: '/ufjf-dcc207-2025-3-a-sem2-amigos-do-sao-pedro-1/',
   plugins: [react()],
 })
